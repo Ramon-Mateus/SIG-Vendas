7ece44b1be4d48e25899b1c1265949c78dc223a8
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-21 13:45:10 -0300

    feat: add products in the database and some types

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,36 @@
-import { NextResponse } from "next/server";
-import { SKUs } from "./../lib/produtosCaseSB";
+// import { NextResponse } from "next/server";
+// import { SKUs } from "./../lib/produtosCaseSB";
  
-export async function GET() {
-    return NextResponse.json(SKUs);
+// export async function GET() {
+//     return NextResponse.json(SKUs);
+// }
+
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+import { SKUs } from "./../lib/produtosCaseSB";
+
+const prisma = new PrismaClient();
+
+export async function POST() {
+    try {
+      for (const item of SKUs) {
+        await prisma.produto.create({
+          data: {
+            SKU: item.SKU,
+            produto: item.produto,
+            preco_cheio: item.preco_cheio,
+            preco_descontado: item.preco_descontado,
+          },
+        });
+      }
+  
+      return NextResponse.json({ message: 'Produtos adicionados com sucesso' });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return NextResponse.json({ message: 'Erro ao adicionar produtos', error: error.message }, { status: 500 });
+      }
+      return NextResponse.json({ message: 'Erro desconhecido' }, { status: 500 });
+    } finally {
+      await prisma.$disconnect();
+    }
 }
--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -5,3 +5,15 @@ export type product = {
     preco_descontado: number,
     quantity?: number
 }
+
+export enum prazo_adicional {
+    padrao = 1,
+    turbo = 2,
+    super_turbo = 3
+}
+
+export enum status_venda {
+    aceita = 1,
+    recusada = 2,
+    analise = 3
+}
