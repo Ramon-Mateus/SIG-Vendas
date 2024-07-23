d3a0c9008ab35cc6df4f697b48f34039af2dac59
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-23 19:49:27 -0300

    feat: ajustes no body

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -19,7 +19,7 @@ export default function Admin() {
   }, []);
 
   return (
-    <div className="flex gap-4 flex-wrap mx-auto max-w-7xl text-slate-300">
+    <div className="flex gap-4 flex-wrap mx-auto max-w-7xl text-slate-300 mb-5">
       {
         vendas.length === 0 ? (
           <p className="text-xl text-slate-300">Nenhuma venda em análise</p>
--- src/app/components/VendaAdmin.tsx
+++ src/app/components/VendaAdmin.tsx
@@ -31,14 +31,14 @@ export function VendaAdmin({ venda }: vendaProps) {
 
     return (
         <div className="w-96 bg-slate-300 h-auto border flex justify-between border-slate-600 py-4 px-2 items-center rounded-md">
-            <span className="font-semibold text-slate-800">Identificar: {venda.id}</span>
+            <span className="font-semibold text-slate-800">Identificador: {venda.id}</span>
             <div>
-            <button onClick={() => handleUpdateStatus(status_venda.aceita)} className="border border-green-600 p-2 rounded-md bg-green-600 hover:bg-green-700 mr-4">
-                Aceitar
-            </button>
-            <button onClick={() => handleUpdateStatus(status_venda.recusada)} className="border border-red-600 p-2 rounded-md bg-red-600 hover:bg-red-700">
-                Recusar
-            </button>
+                <button onClick={() => handleUpdateStatus(status_venda.aceita)} className="border border-green-600 p-2 rounded-md bg-green-600 hover:bg-green-700 mr-4">
+                    Aceitar
+                </button>
+                <button onClick={() => handleUpdateStatus(status_venda.recusada)} className="border border-red-600 p-2 rounded-md bg-red-600 hover:bg-red-700">
+                    Recusar
+                </button>
             </div>
       </div>
     )
