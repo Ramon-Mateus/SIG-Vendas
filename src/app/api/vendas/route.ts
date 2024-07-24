b9d5832f6bebbd8ea4379e88bb88203254ef2f41
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-23 21:12:00 -0300

    feat: rotina que aplica o status da venda na api

--- src/app/api/vendas/route.ts
+++ src/app/api/vendas/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/app/lib/prisma";
-import { product, status_venda } from "@/app/lib/types";
+import { prazo_adicional, product, status_venda } from "@/app/lib/types";
 import { forma_pagamento } from "../../lib/types"
 
 export async function GET() {
@@ -16,17 +16,45 @@ export async function GET() {
 }
 
 function totalVenda(itens: any, frete: number, desconto: number, forma_pagamento_item: number, prazo_adicional: number) {
-    let somaProducts: number;
+    let soma = somaProducts(itens, forma_pagamento_item)
 
+    let total = soma + frete + (soma * prazo_adicional)/100 - desconto;
+
+    return total;
+}
+
+function vendaStatus(desconto: number, frete: number, itens: any, forma_pagamento_item: number, prazo_adicional_item: number) {
+    let soma = somaProducts(itens, forma_pagamento_item)
+
+    if(prazo_adicional_item === prazo_adicional.padrao) {
+        soma = soma * 0.05;
+    } else if(prazo_adicional_item === prazo_adicional.turbo) {
+        soma = soma * 0.10;
+    } else {
+        soma = soma * 0.20;
+    }
+
+    let maior: number;
+
+    soma > frete ? maior = soma : maior = frete;
+
+    if(desconto > maior) {
+        return status_venda.analise
+    }
+
+    return status_venda.aceita
+}
+
+function somaProducts(itens: any, forma_pagamento_item: number) {
+    let somaProducts: number;
+    
     if(forma_pagamento_item === forma_pagamento.cartao_credito) {
         somaProducts = itens.reduce((sum: number, item: product) => (sum + item.preco_cheio * item.quantity!), 0);
     } else {
         somaProducts = itens.reduce((sum: number, item: product) => (sum + item.preco_descontado * item.quantity!), 0);
     }
 
-    let total = somaProducts + frete + (somaProducts * prazo_adicional)/100 - desconto;
-
-    return total;
+    return somaProducts
 }
 
 export async function POST(request: Request) {
@@ -38,7 +66,7 @@ export async function POST(request: Request) {
 
     const venda = await prisma.venda.create({
         data: {
-            status: status_venda.analise,
+            status: vendaStatus(desconto, frete, itens, forma_pagamento, prazo_adicional),
             total: totalVenda(itens, frete, desconto, forma_pagamento, prazo_adicional),
             endereco: endereco,
             frete: frete,
