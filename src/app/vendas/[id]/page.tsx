a0aaedec775bc416ac93af9426d7f0357e9ad58a
Ramon-Mateus <ramonmateus00@gmail.com>
2025-06-22 20:43:32 -0300

    fix: remove default de venda detail

--- src/app/vendas/[id]/page.tsx
+++ src/app/vendas/[id]/page.tsx
@@ -6,7 +6,7 @@ import { venda } from "@/app/lib/types";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react"
 
-export default function VendaDetail(resquest: Request, context: any) {
+export function VendaDetail(resquest: Request, context: any) {
     const { id } = useParams();
 
     const [venda, setVenda] = useState<venda | undefined>();
